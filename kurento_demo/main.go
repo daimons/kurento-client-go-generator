cb1e1decaef74fa1b99373570a8a54663f84d6b7
ChengBin <997155658@qq.com>
2017-03-14 00:43:53 +0800

    added static directory

--- kurento_demo/main.go
+++ kurento_demo/main.go
@@ -1,11 +1,19 @@
 package main
 
 import "kurento-client-go-generator/kurento"
+import "net/http"
 
 var pipeline = new(kurento.MediaPipeline)
 var master = new(kurento.WebRtcEndPoint)
 var server = kurento.NewConnection('ws://127.0.0.1:8888')
 
 func main() {
-	
+	http.HandleFunc("/static/", staticExecute)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func staticExecute(response http.Response, request *http.Request)  {
+	requestUrl := request.URL.String()
+	filepath := requestUrl[len("/static"):]
 }
